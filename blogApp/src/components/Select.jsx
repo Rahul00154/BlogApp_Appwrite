42af9996e245143c8dc046e78f3b66559fcea885
Rahul00154 <103448932+Rahul00154@users.noreply.github.com>
2023-11-15 18:40:35 +0530

    headerpart done

---
+++ blogApp/src/components/Select.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useId } from "react";
+
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id} className=""></label>}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-150 border border-gray-300 w-full ${className}`}
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default React.forwardRef(Select);
